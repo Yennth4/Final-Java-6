78b1cc0a34124bcd46f614ddc06f317fbe8b3997
Hai Yen Nguyen <haiyenng411@gmail.com>
2024-07-30 13:38:23 +0700

    de1

---
+++ src/main/java/com/yennth/ph31906_nguyenhaiyen/Ph31906NguyenHaiYenApplication.java
@@ -0,0 +1,13 @@
+package com.yennth.ph31906_nguyenhaiyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ph31906NguyenHaiYenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ph31906NguyenHaiYenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yennth/ph31906_nguyenhaiyen/Ph31906NguyenHaiYenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yennth.ph31906_nguyenhaiyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ph31906NguyenHaiYenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
